312b2c76e2ef13ce01dc872a8cdf50206b34899c
dd060606 <dd06.developpement@gmail.com>
2025-05-12 15:21:43 +0200

    wip: process requests using Tauri commands in Rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,30 @@
-#[tauri::command]
-fn console_log(msg: String)-> String {
-  println!("I was invoked from JavaScript, with this message: {}", msg);
-  "Hello from Rust!".into()
+mod aurion;
+
+use std::sync::Arc;
+use std::time::Duration;
+use tauri_plugin_http::reqwest::cookie::Jar;
+use tauri_plugin_http::reqwest::Client;
+
+struct HttpClient {
+    client: Client,
+    base_url: String,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let cookie_store = Arc::new(Jar::default());
+    let client = Client::builder()
+        .timeout(Duration::from_secs(8))
+        .cookie_provider(cookie_store)
+        .build()
+        .expect("Failed to build client");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![console_log])
+        .manage(HttpClient {
+            client,
+            base_url: "https://web.isen-ouest.fr/webAurion".to_string(),
+        })
+        .invoke_handler(tauri::generate_handler![aurion::auth::login])
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
