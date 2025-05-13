73999b6de16080bf7a95c34a043db8442aeb4ccc
dd060606 <dd06.developpement@gmail.com>
2025-05-13 12:00:08 +0200

    feat: webAurion requests on Rust side

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,30 +1,43 @@
-mod aurion;
+mod requests;
+mod stores;
 
+use crate::requests::{send_get, send_post};
 use std::sync::Arc;
 use std::time::Duration;
+use tauri::Manager;
 use tauri_plugin_http::reqwest::cookie::Jar;
 use tauri_plugin_http::reqwest::Client;
 
 struct HttpClient {
     client: Client,
-    base_url: String,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let cookie_store = Arc::new(Jar::default());
     let client = Client::builder()
-        .timeout(Duration::from_secs(8))
+        .timeout(Duration::from_secs(10))
         .cookie_provider(cookie_store)
         .build()
         .expect("Failed to build client");
 
     tauri::Builder::default()
-        .manage(HttpClient {
-            client,
-            base_url: "https://web.isen-ouest.fr/webAurion".to_string(),
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .setup(|app| {
+            // Secure Store
+            let salt_path = app
+                .app_handle()
+                .path()
+                .app_local_data_dir()
+                .expect("could not resolve app local data path")
+                .join("salt.txt");
+            app.handle()
+                .plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
+
+            Ok(())
         })
-        .invoke_handler(tauri::generate_handler![aurion::auth::login])
+        .manage(HttpClient { client })
+        .invoke_handler(tauri::generate_handler![send_post, send_get])
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
---
+++ src-tauri/src/requests/mod.rs
@@ -0,0 +1,36 @@
+use crate::HttpClient;
+use std::collections::HashMap;
+use tauri::State;
+
+// Requête POST
+#[tauri::command]
+pub async fn send_post(
+    state: State<'_, HttpClient>,
+    url: String,
+    params: HashMap<String, String>,
+) -> Result<String, String> {
+    let response = state
+        .client
+        .post(url)
+        .form(&params)
+        .send()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let body = response.text().await.map_err(|e| e.to_string())?;
+    Ok(body)
+}
+
+// Requête GET
+#[tauri::command]
+pub async fn send_get(state: State<'_, HttpClient>, url: String) -> Result<String, String> {
+    let response = state
+        .client
+        .get(url)
+        .send()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let body = response.text().await.map_err(|e| e.to_string())?;
+    Ok(body)
+}
